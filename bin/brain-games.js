fac96155e7f5b3c870ea042d4c7ef158fe3f7ef5
Andrei Lebedev <Lebedos773@gmail.com>
2023-04-06 13:04:41 +0300

    fixed isues

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,8 @@
-#! /usr/bin/env node 
+#! /usr/bin/env node
 import readLineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
 
 const name = readLineSync.question('May I have your name? ');
 
-console.log('Hello ' + name + '!');
+console.log(`Hello ${name}!`);
