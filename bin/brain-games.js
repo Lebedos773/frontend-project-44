3eca5a357d8129ae82ac80f84c996fcd2f1dc9bc
Andrei Lebedev <Lebedos773@gmail.com>
2023-04-17 10:16:38 +0300

    aded first question and Math.random

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -6,3 +6,10 @@ console.log('Welcome to the Brain Games!');
 const name = readLineSync.question('May I have your name? ');
 
 console.log(`Hello ${name}!`);
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+const number = Math.random(1, 10);
+
+const firstQuestion = readLineSync.question(number);
+
