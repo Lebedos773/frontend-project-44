d648dcd151fc62423e58dd644d2dc0ca4d1b5c3f
Andrei Lebedev <Lebedos773@gmail.com>
2023-04-20 12:05:10 +0300

    saved cli.js

--- src/cli.js
+++ src/cli.js
@@ -3,3 +3,7 @@ import readLineSync from 'readline-sync';
 const name = readLineSync.question('May I have your name? ');
 
 export default name;
+
+const number = Math.floor(Math.random(1, 10));
+
+export default number;
