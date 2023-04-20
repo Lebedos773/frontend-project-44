379cab6fdf3dce2e032af6314e97affc46be9fa6
Andrei Lebedev <Lebedos773@gmail.com>
2023-04-20 15:23:58 +0300

    ewerithing works

---
+++ bin/brain-even.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import readLineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+
+const name = readLineSync.question('May I have your name? ');
+
+console.log(`Hello ${name}!`);
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+function ascking() {
+  let i = 0;
+
+  while (i < 3) {
+    const number = Math.round(Math.random(10) * 10);
+    const question = readLineSync.question(`Question: ${number}`);
+    console.log(`Your answer: ${question}`);
+
+    if (number % 2 === 0 && question === 'yes') {
+      i += 1;
+      console.log('Correct!');
+    } else if (number % 2 !== 0 && question === 'no') {
+      i += 1;
+      console.log('Correct!');
+    } else {
+      const wrongYes = `${question} is wrong answer ;(. correct answer was 'no'.\nLet's try again ${name}!`;
+      const wrongNo = `${question} is wrong answer ;(. correct answer was 'yes'.\nLet's try again ${name}!`;
+
+      return question === 'no' ? console.log(wrongNo) : console.log(wrongYes);
+    }
+  }
+  return console.log(`Congratulations, ${name}`);
+}
+
+ascking()
