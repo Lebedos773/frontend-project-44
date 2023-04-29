b9b752ba8db028b51e9dfd6ce5c3ec6f9ef614d3
Andrei Lebedev <Lebedos773@gmail.com>
2023-04-29 10:54:39 +0300

    fixed file index.js

--- src/index.js
+++ src/index.js
@@ -44,3 +44,16 @@ export function threeIteration(answ, result, name) {
   }
   return console.log(`Congratulations ${name}!`);
 }
+
+export function progression() {
+  let arr1 = [];
+
+  const rndmNum = Math.round(Math.random() * 100);
+  const rndmStep = Math.round(Math.random() * 5);
+
+  for (let i = 0; i < rndmStep * 10; i += rndmStep) {
+    arr1.push(rndmNum + i);
+  }
+
+  return console.log(arr1);
+}
