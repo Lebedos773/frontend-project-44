0c6a3c098446c821aa748ef313e6549fefc1ddf7
Andrei Lebedev <Lebedos773@gmail.com>
2023-04-29 10:55:41 +0300

    the game brain-progressive works

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,9 +1,54 @@
 #! /usr/bin/env node
 
-import readLineSynk from 'readline-sync';
-import { makeProgression } from '../src/index.js';
+import readLineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+
+const name = readLineSync.question('May I have your name? ');
+
+console.log(`Hello ${name}!`);
+console.log('What number is missing in the progression?.');
 
 
+const makeProgression = () => {
+  let result = [];
+
+  const rndmNum = Math.round(Math.random() * 100);
+  const rndmStep = Math.round(Math.random() * 5);
+
+  for (let i = 0; i < rndmStep * 10; i += rndmStep) {
+    result.push(rndmNum + i);
+  }
+
+  return result;
+}
 
 
-console.log(makeProgression());
+function ascking() {
+  let i = 0;
+
+  while (i < 3) {
+  
+    const arr1 = makeProgression();
+    const getRandom = Math.round(Math.random() * 10);
+    const rightAnswer = arr1[getRandom];
+    const arr2 = arr1;
+    arr2[getRandom] = '..';
+
+
+    console.log(`Question: ${arr2}`);
+    const userAnswer = readLineSync.question('Your answer: ');
+   
+    if (Number(userAnswer) === rightAnswer) {
+      i += 1;
+      console.log('Correct!');
+    } else {
+      return console.log(`${userAnswer} is wrong answer ;(. correct answer was ${rightAnswer}.\nLet's try again ${name}!`);
+    }
+  }
+  
+  return console.log(`Congratulations, ${name}!`);
+}
+
+
+ascking()
